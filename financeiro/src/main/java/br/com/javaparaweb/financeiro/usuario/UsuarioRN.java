3334fa573f95a9d15d0899f85e9e0fec320a4379
JhonisAnjos <38966024+JhonisAnjos@users.noreply.github.com>
2018-09-04 12:01:23 -0300

    Primeira versão do projeto

---
+++ financeiro/src/main/java/br/com/javaparaweb/financeiro/usuario/UsuarioRN.java
@@ -0,0 +1,33 @@
+package br.com.javaparaweb.financeiro.usuario;
+
+import java.util.List;
+
+import br.com.javaparaweb.financeiro.util.DAOFactory;
+
+public class UsuarioRN {
+	private UsuarioDAO usuarioDAO;
+	public UsuarioRN(){
+		this.usuarioDAO=DAOFactory.criarUsuarioDAO();
+	}
+	public Usuario carregar(Integer codigo){
+		return this.usuarioDAO.carregar(codigo);
+	}
+	public Usuario buscarPorLogin(String login){
+	    return this.usuarioDAO.buscarPorLogin(login);
+	}
+	public void Salvar(Usuario usuario){
+		Integer codifo= usuario.getCodigo();
+		if(codifo==null||codifo==0){
+			this.usuarioDAO.salvar(usuario);
+		}else{
+			this.usuarioDAO.atualizar(usuario);
+		}
+	}
+	public void excluir(Usuario usuario){
+		this.usuarioDAO.excluir(usuario);
+	}
+	public List<Usuario> listar(){
+		return this.usuarioDAO.listar();
+	}
+
+}
